a5a9197068ab32af2f5f1b0ff1e5f4485665cc3c
Ihsaan de Villiers <ihsaancput@gmail.com>
2017-08-08 12:43:03 +0200

    Add files via upload

---
+++ Athletics/README.md
@@ -0,0 +1,2 @@
+"# Althletics" 
+"# Althletics" 
---
+++ Athletics/src/main/java/co/za/midv1990/Domain/Athlete.java
@@ -0,0 +1,66 @@
+package co.za.midv1990.Domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Created by Ihsaan on 2017/08/08.
+ */
+public class Athlete implements Serializable {
+    private String name,surname,identity;
+    private int age;
+
+    //getters
+    public String getName() {return name;}
+    public String getSurname() {return surname;}
+    public String getIdentity() {return identity;}
+    public int getAge() {return age;}
+
+    //constructor
+    public Athlete(Builder build) {
+        this.name = build.name;
+        this.surname = build.surname;
+        this.identity = build.identity;
+        this.age = build.age;
+    }
+
+    //builder class
+    public static class Builder
+    {
+        private String name,surname,identity;
+        private int age;
+
+        public Builder name(String value)
+        {
+            this.name = value;
+            return this;
+        }
+        public Builder surname(String value)
+        {
+            this.surname = value;
+            return this;
+        }
+        public Builder identity(String value)
+        {
+            this.identity = value;
+            return this;
+        }
+        public Builder age(int value)
+        {
+            this.age = value;
+            return this;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Athlete athlete = (Athlete) o;
+
+        return name.equals(athlete.name);
+    }
+    @Override
+    public int hashCode() {return name.hashCode();}
+}
